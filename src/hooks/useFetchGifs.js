974a4852d5a4ac3765bf34f18aa7dcdb43bee0f7
normanrb91 <normanromano16@gmail.com>
2023-10-09 17:59:17 +0200

    create custom hook useFetchGifs

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+import { getGifs } from "../helper/getGifs"
+
+
+export const useFetchGifs = (category) => {
+
+    const [state, setState] = useState({
+        data: [],
+        loading : true
+    })
+
+    useEffect(() => {
+
+        getGifs()
+        .then(gifs => {
+            setState({
+                data: gifs,
+                loading: false
+            })
+        })
+        .catch(() => {
+            setState({
+                ...data,
+                loading: false
+            })
+        })
+
+    }, [category])
+    
+    return state
+}
